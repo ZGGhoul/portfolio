547a6001b600416dbd24df866d9ac961b408b652
ZGGhoul <134587569+ZGGhoul@users.noreply.github.com>
2025-03-22 16:54:40 +0100

    Formázás part 1

--- main.js
+++ main.js
@@ -8,12 +8,17 @@ function renderPortfolio(data){
     data.forEach(({id, title, photo_url, repo_link, topics, site_link})=>{
         document.getElementById("work").innerHTML +=`
         <div id="portfolio">
+                <div id="kepdiv">
+                <img src="${photo_url}" alt="" id="munkakep">
+                </div>
+                
+                <div id="infodiv">
                 <h1>${title}</h1>
-                <img src="${photo_url}" alt="">
-                <h3>${topics}</h3>
-                <a href="${site_link}"></a>
-                <a href="${repo_link}"></a>
-
+                <h3>Topics</h3>
+                <ul>${topics}</ul>
+                <a href="${site_link}">Site Link</a>
+                <a href="${repo_link}">Repo Link</a>
+                </div>
             </div>
         `
     })
